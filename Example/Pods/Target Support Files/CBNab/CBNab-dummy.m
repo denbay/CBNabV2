9690d9b0ebe805c576a7e35b6bf8050de3073387
Dzianis Baidan <dracmor@ya.ru>
2020-06-04 01:19:17 +0300

    Init

---
+++ Example/Pods/Target Support Files/CBNab/CBNab-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CBNab : NSObject
+@end
+@implementation PodsDummy_CBNab
+@end
---
+++ Example/Pods/Target Support Files/Pods-CBNab_Example/Pods-CBNab_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CBNab_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CBNab_Example
+@end
